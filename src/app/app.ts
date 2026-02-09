390370f68179f3b87acae5be21716565e8a464cb
David Sandoval <davidsandoval.dev@gmail.com>
2026-02-08 22:31:41 -0500

    🎨 style(app): apply prettier formatting

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
-  styles: `
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-  `,
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	template: `<router-outlet />`,
+	styles: `
+		:host {
+			max-width: 1280px;
+			margin: 0 auto;
+			padding: 2rem;
+			text-align: center;
+		}
+	`,
 })
 export class App {}
--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 import { AnalogWelcome } from './analog-welcome';
 
 @Component({
-  selector: 'app-home',
-  imports: [AnalogWelcome],
-  template: `
-     <app-analog-welcome/>
-  `,
+	selector: 'app-home',
+	imports: [AnalogWelcome],
+	template: ` <app-analog-welcome /> `,
 })
 export default class Home {}
