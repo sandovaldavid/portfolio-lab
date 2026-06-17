15a759aba5c928778decda9c0ee21355161d9048
David Sandoval <dev@sandovaldavid.com>
2026-06-17 17:36:31 -0500

    🔧 config(vite): add csharp to prism additional languages

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig(({ mode }) => ({
     analog({
       content: {
         highlighter: 'prism',
+        prismOptions: {
+          additionalLangs: ['csharp'],
+        },
       },
       prerender: {
         routes: [
